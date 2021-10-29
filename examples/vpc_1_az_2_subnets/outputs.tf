2e2b0c30abed7655ca6bbc12e333d6058e13b1a1
Max <max.teiger@gmail.com>
2021-10-29 10:31:49 +0200

    fix: typo in outputs.tf in simple example

--- examples/vpc_1_az_2_subnets/outputs.tf
+++ examples/vpc_1_az_2_subnets/outputs.tf
@@ -5,12 +5,12 @@ output "vpc_id" {
 }
 
 # Subnets
-output "private_subnets_ids" {
-  description = "List of IDs of private subnets"
-  value       = module.simple_vpc.private_subnets_id
-}
-
 output "public_subnets_ids" {
   description = "List of IDs of public subnets"
   value       = module.simple_vpc.public_subnets_id
+}
+
+output "private_subnets_ids" {
+  description = "List of IDs of private subnets"
+  value       = module.simple_vpc.private_subnets_id
 }
