0b18799d799ed765a56d1f27596bfa94b2f5c5b6
Matthieu L <matthieu@lanvert.com>
2022-01-14 16:28:28 +0100

    Update outputs.tf
    vpc -> this

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,26 @@
 # VPC ID
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+  value       = module.this.vpc_id
 }
 
 # Subnets ID
 output "public_subnets_ids" {
   description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
+  value       = module.this.public_subnets
 }
 
 output "private_subnets_ids" {
   description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
+  value       = module.this.private_subnets
 }
 
 output "intra_subnets_ids" {
   description = "List of IDs of intra subnets"
-  value       = module.vpc.intra_subnets
+  value       = module.this.intra_subnets
 }
 
 output "nat_gateway_ips" {
   description = "List of Nat Gateway External IPs"
-  value       = module.vpc.nat_public_ips
+  value       = module.this.nat_public_ips
 }
