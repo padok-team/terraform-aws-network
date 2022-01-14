935ede10a874c25ee7ceed79fdd5dc76f1fd5b64
Remi <remip@padok.fr>
2022-01-14 14:37:49 +0100

    feat(community-compliance): work on names and variables

--- variables.tf
+++ variables.tf
@@ -1,65 +1,65 @@
 # Global VPC variables
 variable "vpc_name" {
-  type        = string
   description = "Name of your VPC"
+  type        = string
 }
 
 variable "vpc_cidr" {
-  type        = string
   description = "CIDR block of your VPC"
+  type        = string
 }
 
 variable "vpc_availability_zone" {
-  type        = list(string)
   description = "List of AZ Names or ID in your region"
+  type        = list(string)
 }
 
 variable "map_public_ip_on_launch" {
-  type        = bool
   description = "Associate public IP to instances by default"
+  type        = bool
   default     = false
 }
 
 variable "enable_nat_gateway" {
-  type        = bool
   description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+  type        = bool
   default     = true
 }
 
 variable "single_nat_gateway" {
-  type        = bool
   description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks. Set to false for production environment"
+  type        = bool
   default     = true
 }
 
 variable "create_igw" {
-  type        = bool
   description = "Controls if an Internet Gateway is created for public subnets and the related routes that connect them."
+  type        = bool
   default     = true
 }
 
 # Global tags
 variable "tags" {
-  type        = map(string)
   description = "Tags for all your ressources"
+  type        = map(string)
 }
 
 variable "vpc_tags" {
-  type        = map(string)
   description = "Additional Tags for your VPC"
+  type        = map(string)
   default     = {}
 }
 
 # Public subnets information
 variable "public_subnet_cidr" {
-  type        = list(string)
   description = "List of public CIDR to deploy in your VPC"
+  type        = list(string)
   default     = []
 }
 
 variable "public_subnet_suffix" {
-  type        = string
   description = "Suffix for your public subnets"
+  type        = string
   default     = "public"
 }
 
@@ -81,30 +81,28 @@ variable "public_outbound_acl_rules" {
   default     = []
 }
 
-
 variable "public_subnet_tags" {
-  type        = map(string)
   description = "Additional Tags for your public subnets"
+  type        = map(string)
   default     = {}
 }
 
-
 variable "public_acl_tags" {
-  type        = map(string)
   description = "Additional Tags for your private acl"
+  type        = map(string)
   default     = {}
 }
 
 # Private subnets information
 variable "private_subnet_cidr" {
-  type        = list(string)
   description = "List of Private CIDR to deploy in your VPC"
+  type        = list(string)
   default     = []
 }
 
 variable "private_subnet_suffix" {
-  type        = string
   description = "Suffix for your public subnets"
+  type        = string
   default     = "private"
 }
 
@@ -127,27 +125,27 @@ variable "private_outbound_acl_rules" {
 }
 
 variable "private_subnet_tags" {
-  type        = map(string)
   description = "Additional Tags for your private subnets"
+  type        = map(string)
   default     = {}
 }
 
 variable "private_acl_tags" {
-  type        = map(string)
   description = "Additional Tags for your private acl"
+  type        = map(string)
   default     = {}
 }
 
 # Intra subnets information
 variable "intra_subnet_cidr" {
-  type        = list(string)
   description = "List of intra CIDR to deploy in your VPC"
+  type        = list(string)
   default     = []
 }
 
 variable "intra_subnet_suffix" {
-  type        = string
   description = "Suffix for your public subnets"
+  type        = string
   default     = "intra"
 }
 
@@ -170,13 +168,13 @@ variable "intra_outbound_acl_rules" {
 }
 
 variable "intra_subnet_tags" {
-  type        = map(string)
   description = "Additional Tags for your intra subnets"
+  type        = map(string)
   default     = {}
 }
 
 variable "intra_acl_tags" {
-  type        = map(string)
   description = "Additional Tags for your intra acl"
+  type        = map(string)
   default     = {}
 }
